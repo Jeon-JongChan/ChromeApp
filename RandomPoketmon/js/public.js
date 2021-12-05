06bd04727d28998b3bc315f895f20799dd9fb894
tab <loki377312@gmail.com>
2021-12-05 16:03:55 +0900

    랜덤포켓몬 - 2차완성

--- RandomPoketmon/js/public.js
+++ RandomPoketmon/js/public.js
@@ -7,8 +7,8 @@ function getDomIndex(dom) {
     var elem = dom.parentNode;
     var idx = null;
     for(var i = 0; i < elem.childNodes.length; i++) {
-      if (elem.parentNode.childNodes[i] === dom) {
-        console.log('elemIndex = ' + i);
+      if (elem.childNodes[i] === dom) {
+        //console.log(elem, elem.childNodes[i],'elemIndex = ', i);
         idx = i;
         break;
       }
@@ -18,10 +18,22 @@ function getDomIndex(dom) {
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //최댓값도 포함, 최솟값도 포함
+    return Math.floor(Math.random() * (max - min)) + min; //최댓값도 미포함, 최솟값만 포함
 }
 
 function getRandomValue(values) {
     let idx = getRandomInt(0, values.length);
     return values[idx];
+}
+function copyToClipBoard(query) {
+    var content = document.querySelector(query).innerText;
+
+    navigator.clipboard.writeText(content)
+        .then(() => {
+        alert("텍스트만 복사되었습니다.");
+        console.log("Text copied to clipboard...")
+    })
+        .catch(err => {
+        console.log('Something went wrong', err);
+    })
 }
